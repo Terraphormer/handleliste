9d6bd0d338d95eef7ae92bf61a6cba9c35899642
Terraphormer <mortentangjerd@gmail.com>
2022-10-09 22:07:11 +0200

    modified:   css/style.css 	modified:   index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
 
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
-    header("location: usersystem/signup.php");
+    header("location: usersystem/login.php");
     exit;
 } else {
     header("location: handleliste.php");
